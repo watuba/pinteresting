79ba0ea9bf9bb40f358e3f957213ac1df9d4cdc4
B Haun <bhauncap@gmail.com>
2014-06-19 17:52:37 +0200

    try this

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	do_not_validate_attachment_file_type :image
 	attr_accessor :image_file_name
 	attr_accessor :image_content_type
