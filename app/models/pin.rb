4e4ad7907fcf8d936d7d1bf77a57d3a29c452f16
B Haun <bhauncap@gmail.com>
2014-06-19 17:39:28 +0200

    please work

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, 
-						:styles => { :medium => "300x300>", :thumb => "100x100>" },
-						:storage => :s3,
-						:bucket => ENV['AWS_BUCKET_NAME'],
-  						:s3_credentials => {
-						    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-						    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-						  }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
 	do_not_validate_attachment_file_type :image
 	attr_accessor :image_file_name
 	attr_accessor :image_content_type
