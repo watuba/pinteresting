f6504477cfda3ab1e272b74dab49ba18eb6a1e45
B Haun <bhauncap@gmail.com>
2014-06-19 17:41:12 +0200

    please work again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	do_not_validate_attachment_file_type :image
 	attr_accessor :image_file_name
 	attr_accessor :image_content_type
