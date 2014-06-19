c507a2a78df549eb96257531fa65a9e7ff2d3d44
B Haun <bhauncap@gmail.com>
2014-06-19 16:51:20 +0200

    made change to pin Model to troubleshoot heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	do_not_validate_attachment_file_type :image
+	attr_accessor :photo_file_name
+	attr_accessor :photo_content_type
+	attr_accessor :photo_file_size
+	attr_accessor :photo_updated_at
 end
