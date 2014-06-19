54647cf3e4b441e95000e96ac7ab905dfd3ee2dd
B Haun <bhauncap@gmail.com>
2014-06-19 17:56:06 +0200

    halleluja

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,8 +3,5 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	do_not_validate_attachment_file_type :image
-	attr_accessor :image_file_name
-	attr_accessor :image_content_type
-	attr_accessor :image_file_size
-	attr_accessor :image_updated_at
+
 end
