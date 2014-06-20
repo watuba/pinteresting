43a1305696477531e18314781e7bd4a023260d88
B Haun <bhauncap@gmail.com>
2014-06-20 13:52:51 +0200

    add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	do_not_validate_attachment_file_type :image
 
+	validates :image, presence: true
+  	validates :description, presence: true
+
 end
